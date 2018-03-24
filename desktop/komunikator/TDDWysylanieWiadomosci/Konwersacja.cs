e0f4f1b7d713536698546135e049c2d77437dbe3
bprusaczyk <bartoszprusaczyk557@gmail.com>
2018-03-24 22:04:55 +0100

    Utworzenie klasy Konwersacja w aplikacji desktopowej

---
+++ desktop/komunikator/TDDWysylanieWiadomosci/Konwersacja.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace komunikator
+{
+    namespace wysylanieWiadomosci
+    {
+        class Konwersacja
+        {
+            private int wczytaneWiadomosci;
+            private MySqlConnection polaczenie;
+
+            public Konwersacja()
+            {
+                polaczZBaza();
+                wczytaneWiadomosci = 0;
+            }
+
+            private void polaczZBaza()
+            {
+                polaczenie = new MySqlConnection("Server=localhost; database=komunikator; UID=root; password=");
+                polaczenie.Open();
+            }
+
+            public string znajdzIdUzytkownika(string login)
+            {
+                MySqlCommand zapytanie = polaczenie.CreateCommand();
+                zapytanie.CommandText = "select idUzytkownika from uzytkownicy where login='" + login + "'";
+                MySqlDataReader wynik = zapytanie.ExecuteReader();
+                string id=null;
+                while (wynik.Read())
+                {
+                    id = wynik["idUzytkownika"].ToString();
+                }
+                wynik.Close();
+                return id;
+            }
+
+            public void wyslijWiadomosc(string uzytkownikWysylajacy, string adresat, string tresc)
+            {
+                MySqlCommand polecenie = polaczenie.CreateCommand();
+                //polecenie.CommandText="insert into wiadomosci"
+            }
+
+            public string[] wczytajWiadomosci(string uzytkownikWysylajacy, string adresat)
+            {
+
+                return null;
+            }
+
+            private class Wiadomosc
+            {
+                string tresc;
+                DateTime data;
+                TimeSpan godzina;
+            }
+        }
+    }
+    
+}
---
+++ desktop/komunikator/TDDWysylanieWiadomosci/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using komunikator.wysylanieWiadomosci;
+
+namespace TDDWysylanieWiadomosci
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Konwersacja k = new Konwersacja();
+            string wynik1 = k.znajdzIdUzytkownika("uzytkownik1");
+            string wynik2 = k.znajdzIdUzytkownika("uzytkownik2");
+            string wynik3 = k.znajdzIdUzytkownika("uzytkownik3");
+            Assert.AreEqual("1", wynik1);
+            Assert.AreEqual("2", wynik2);
+            Assert.AreEqual("3", wynik3);
+        }
+    }
+}
