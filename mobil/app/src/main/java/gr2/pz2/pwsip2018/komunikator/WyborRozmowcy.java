3139b990304384886ffe6daef618251882dbf2df
bprusaczyk <bartoszprusaczyk557@gmail.com>
2018-04-03 19:24:22 +0200

    Praca nad interfejsem wyboru konwersacji w aplikacji mobilnej

---
+++ mobil/app/src/main/java/gr2/pz2/pwsip2018/komunikator/WyborRozmowcy.java
@@ -0,0 +1,13 @@
+package gr2.pz2.pwsip2018.komunikator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WyborRozmowcy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wybor_rozmowcy);
+    }
+}
