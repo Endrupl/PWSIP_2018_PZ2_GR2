42e96841b9be9f4b837b3a2c9e6bab5886ab9912
bprusaczyk <bartoszprusaczyk557@gmail.com>
2018-04-06 13:08:41 +0200

    Praca nad wyświetlaniem kontaktów użytkownika w aplikacji mobilnej

--- mobil/app/src/main/java/gr2/pz2/pwsip2018/komunikator/WyborRozmowcy.java
+++ mobil/app/src/main/java/gr2/pz2/pwsip2018/komunikator/WyborRozmowcy.java
@@ -1,13 +1,23 @@
 package gr2.pz2.pwsip2018.komunikator;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class WyborRozmowcy extends AppCompatActivity {
+import gr2.pz2.pwsip2018.komunikator.wysylanieWiadomosci.Konwersacja;
+
+public class WyborRozmowcy extends AppCompatActivity
+{
+    private String zalogowanyUzytkownik = "uzytkownik1";//tymczasowe założenie, że zalogowany użytkownik to uzytkownik1
+    private ListView kontakty;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wybor_rozmowcy);
+
+        Konwersacja.zaladujKontakty(zalogowanyUzytkownik);
     }
 }
