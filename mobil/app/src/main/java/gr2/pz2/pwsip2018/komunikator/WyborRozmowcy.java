a18c1d928498bc670c1912851545468052f8b177
bprusaczyk <bartoszprusaczyk557@gmail.com>
2018-04-12 16:37:17 +0200

    Poprawienie drobnych błędów w aplikacji mmobilnej

--- mobil/app/src/main/java/gr2/pz2/pwsip2018/komunikator/WyborRozmowcy.java
+++ mobil/app/src/main/java/gr2/pz2/pwsip2018/komunikator/WyborRozmowcy.java
@@ -3,8 +3,11 @@ package gr2.pz2.pwsip2018.komunikator;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.List;
 
 import gr2.pz2.pwsip2018.komunikator.wysylanieWiadomosci.Konwersacja;
 
@@ -17,7 +20,8 @@ public class WyborRozmowcy extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wybor_rozmowcy);
-
-        Konwersacja.zaladujKontakty(zalogowanyUzytkownik);
+        List<String> kontaktyUzytkownika = Konwersacja.zaladujKontakty(zalogowanyUzytkownik);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_2, android.R.id.text1, kontaktyUzytkownika);
+        kontakty.setAdapter(adapter);
     }
 }
