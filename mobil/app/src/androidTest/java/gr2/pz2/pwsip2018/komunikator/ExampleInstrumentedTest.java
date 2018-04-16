9fd36098558f377349da2dea35cb82c64252c94c
bprusaczyk <bartoszprusaczyk557@gmail.com>
2018-04-16 16:52:30 +0200

    Dodanie funkcji wyświetlania kontaktów w aplikacji mobilnej

--- mobil/app/src/androidTest/java/gr2/pz2/pwsip2018/komunikator/ExampleInstrumentedTest.java
+++ mobil/app/src/androidTest/java/gr2/pz2/pwsip2018/komunikator/ExampleInstrumentedTest.java
@@ -7,6 +7,8 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import gr2.pz2.pwsip2018.komunikator.wysylanieWiadomosci.Konwersacja;
+
 import static org.junit.Assert.*;
 
 /**
@@ -22,5 +24,26 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("gr2.pz2.pwsip2018.komunikator", appContext.getPackageName());
+    }
+
+    public void znajdzIdUzytkownikaTest()
+    {
+        String wynik1= Konwersacja.znajdzIdUzytkownika("uzytkownik1");
+        String wynik2= Konwersacja.znajdzIdUzytkownika("uzytkownik2");
+        String wynik3= Konwersacja.znajdzIdUzytkownika("uzytkownik3");
+        assertEquals("1", wynik1);
+        assertEquals("2", wynik2);
+        assertEquals("3", wynik3);
+    }
+
+    @Test
+    public void  znajdzUzytkownikaPoIdTest()
+    {
+        String wynik1=Konwersacja.znajdzUzytkownikaPoId("1");
+        String wynik2=Konwersacja.znajdzUzytkownikaPoId("2");
+        String wynik3=Konwersacja.znajdzUzytkownikaPoId("3");
+        assertEquals("uzytkownik1", wynik1);
+        assertEquals("uzytkownik2", wynik2);
+        assertEquals("uzytkownik3", wynik3);
     }
 }
--- mobil/app/src/main/java/gr2/pz2/pwsip2018/komunikator/wysylanieWiadomosci/Konwersacja.java
+++ mobil/app/src/main/java/gr2/pz2/pwsip2018/komunikator/wysylanieWiadomosci/Konwersacja.java
@@ -1,5 +1,7 @@
 package gr2.pz2.pwsip2018.komunikator.wysylanieWiadomosci;
 
+import android.os.StrictMode;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -10,9 +12,9 @@ public class Konwersacja
 {
     public String login;
     public String adresat;
-    private static String daneBazy="jdbc:mysql://192.168.1.16:3306/komunikator";//ze względu na brak serwera trzeba za każdym razem zmienić IP na IP komputera z bazą danych
-    private static String uzytkownikBazy="root";
-    private static String hasloBazy="";
+    private static final String DANE_BAZY ="jdbc:mysql://192.168.1.16:3306/komunikator";//ze względu na brak serwera trzeba za każdym razem zmienić IP na IP komputera z bazą danych
+    private static final String UZYTKOWNIK_BAZY ="root";
+    private static final String HASLO_BAZY ="";
 
     public Konwersacja(String login, String adresat)
     {
@@ -20,14 +22,29 @@ public class Konwersacja
         this.adresat = adresat;
     }
 
+    private static void przygotujDoPolaczeniaZBaza()
+    {
+        StrictMode.ThreadPolicy tp = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(tp);
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+        }
+        catch (Exception e)
+        {
+
+        }
+    }
+
     public static String znajdzIdUzytkownika(String login)
     {
+        przygotujDoPolaczeniaZBaza();
         String id = null;
         Connection polaczenie;
         Statement st;
         try
         {
-            polaczenie= DriverManager.getConnection(daneBazy, uzytkownikBazy, hasloBazy);
+            polaczenie= DriverManager.getConnection(DANE_BAZY, UZYTKOWNIK_BAZY, HASLO_BAZY);
             st=polaczenie.createStatement();
             ResultSet wynik=st.executeQuery("select idUzytkownika from uzytkownicy where login='" + login + "'");
             while (wynik.next())
@@ -44,10 +61,11 @@ public class Konwersacja
 
     public static String znajdzUzytkownikaPoId(String id)
     {
+        przygotujDoPolaczeniaZBaza();
         String uzytkownik=null;
         try
         {
-            Connection polaczenie=DriverManager.getConnection(daneBazy, uzytkownikBazy, hasloBazy);
+            Connection polaczenie=DriverManager.getConnection(DANE_BAZY, UZYTKOWNIK_BAZY, HASLO_BAZY);
             Statement st=polaczenie.createStatement();
             ResultSet wynik=st.executeQuery("select login from uzytkownicy where idUzytkownika='" + id + "'");
             while (wynik.next())
@@ -64,11 +82,12 @@ public class Konwersacja
 
     public static ArrayList<String> zaladujKontakty(String login)
     {
+        przygotujDoPolaczeniaZBaza();
         ArrayList<String> kontakty = new ArrayList<String>();
         String id = znajdzIdUzytkownika(login);
         try
         {
-            Connection polaczenie = DriverManager.getConnection(daneBazy, uzytkownikBazy, hasloBazy);
+            Connection polaczenie = DriverManager.getConnection(DANE_BAZY, UZYTKOWNIK_BAZY, HASLO_BAZY);
             Statement st=polaczenie.createStatement();
             ResultSet wynik=st.executeQuery("select idUzytkownika1, idUzytkownika2 from kontakty where idUzytkownika1=" + id + " or idUzytkownika2=" + id);
             while(wynik.next())
